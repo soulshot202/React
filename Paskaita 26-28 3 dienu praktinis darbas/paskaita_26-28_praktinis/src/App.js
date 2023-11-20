6e6a1b0a2ef7ac3eec76f5398f04a5985b43c871
Vytautas Berzvinskas <misionierius@gmail.com>
2023-11-20 19:45:11 +0200

    s

--- Paskaita 26-28 3 dienu praktinis darbas/paskaita_26-28_praktinis/src/App.js
+++ Paskaita 26-28 3 dienu praktinis darbas/paskaita_26-28_praktinis/src/App.js
@@ -1,20 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
-// import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 function App() {
   const [Open, setOpen] = useState(false);
-  // const [Name, setName] = useState("");
-  // const [Email, setEmail] = useState("");
-  // const [Password, setPassword] = useState("");
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   if (!Name || !Email || !Password) {
-  //     return alert("Fill in all the fields");
-  //   }
-  //   alert("registration success");
-  // }
+
   return (
     <div>
       <div className="open">
